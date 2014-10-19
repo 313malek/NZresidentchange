e1c52a6e6f0d7a846460150387b421598b44546c
Timo Franz <timo@dumpark.com>
2014-10-19 21:54:21 +1300

    adding fullscreen cleanup

--- js/app.js
+++ js/app.js
@@ -1,67 +1,143 @@
 $(function(){
-  L.mapbox.accessToken = 'pk.eyJ1IjoidG1mcm56IiwiYSI6IkRNZURKUHcifQ._ljgPcF75Yig1Of8adL93A';
-  var map = L.map('map',{
-          minZoom: 5,
-          maxZoom: 12,
-          maxBounds: [[-48.6619,155],[-33.2479,190]]
-        }).setView([-41.20,175], 6);
-  var layers = document.getElementById('menu-ui');
 
-  var layer_young=L.mapbox.tileLayer('tmfrnz.l0ffajor'),
-    layer_total=L.mapbox.tileLayer('tmfrnz.jded0a4i'),
-    layer_base =L.mapbox.tileLayer('tmfrnz.pmu6jemi');
-
-  layer_base.setZIndex(1)
-      .addTo(map);
-
-  addLayer(layer_young, 'Aged 20-34', 2);
-  addLayer(layer_total, 'All ages', 3);
+  var map_url = 'http://dumparkltd.github.io/NZresidentchange/',
+      parent_url = 'http://localhost:8888/NZresidentchange/embed.html';      
   
-  $('.toggle-info').on('click',function(e){
-	e.preventDefault();
-	if ($('.information').hasClass('hidden')){
-		$('.information').removeClass('hidden');
-	}else{
-		$('.information').addClass('hidden');
-	}
-	});
-	
-  $('.information').on('click',function(e){
-	e.preventDefault();
-	$('.information').addClass('hidden');
-  })
+  initFullscreen();
   
-  function addLayer(layer, name, zIndex) {
-      layer.setZIndex(zIndex);
+  initMap();
 
-      // Create a simple layer switcher that
-      // toggles layers on and off.
-      var link = document.createElement('a');
-          link.href = '#';
-          link.innerHTML = name;
+  initFooter();
+  
+  
+  // EVENTS
+  function initFooter(){ 
+    $('.toggle-info').on('click',function(e){
+      e.preventDefault();
+      if ($('.information').hasClass('hidden')){
+        $('.information').removeClass('hidden');
+      }else{
+        $('.information').addClass('hidden');
+      }
+    });
 
-    if (name=='Aged 20-34'){
-      layer.addTo(map);
-      link.className = 'active';}
-    else{
-      link.className = ''};
-
-      link.onclick = function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          if (!map.hasLayer(layer)) {
-        map.removeLayer(layer_young);
-        map.removeLayer(layer_total);
-              map.addLayer(layer);
-        $('.active').removeClass('active');
-              this.className = 'active';
-          }
-      };
-
-      layers.appendChild(link);
+    $('.information').on('click',function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.information').addClass('hidden');
+    })
+  }
+  function initMap() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoidG1mcm56IiwiYSI6IkRNZURKUHcifQ._ljgPcF75Yig1Of8adL93A';
+    var map = L.map('map',{
+            minZoom: 5,
+            maxZoom: 12,
+            maxBounds: [[-48.6619,155],[-33.2479,190]],
+            loadingControl: true
+          }).setView([-41.20,175], 6);
+    
+    // add basemap
+    L.mapbox.tileLayer('tmfrnz.pmu6jemi')
+            .setZIndex(1)
+            .addTo(map);
+    
+    // add young map
+    var layer_young = L.mapbox.tileLayer('tmfrnz.l0ffajor')
+            .setZIndex(2)
+            .addTo(map);
+    $("a[data-mapid='y']").addClass('active');
+            
+    
+    // add     
+    var layer_total = L.mapbox.tileLayer('tmfrnz.jded0a4i')
+      .setZIndex(2);
+    
+    $('.toggle-layer').on('click', function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      var data = $(this).data();
+      if (!$(this).hasClass('active')){
+        $('.toggle-layer').removeClass('active');        
+        if (data.mapid === 'y') {
+          map.removeLayer(layer_total);
+          map.addLayer(layer_young);
+        } else if (data.mapid === 'all') {
+          map.removeLayer(layer_young);
+          map.addLayer(layer_total);
+        }
+        $(this).addClass('active');
+      }      
+    });
   }
   
   
+/* FULLSCREEN   ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+  function initFullscreen() {
+    // if embedded offer fullscreen
+    if (top !== self) {
+      $('body').removeClass('standalone');
+      var wasFullScreen = fullScreenApi.isFullScreen(),
+        resizeTimer;
+      $(window).on('resize', function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(resized, 300);
+      });
+
+      function resized() {
+        if (wasFullScreen !== fullScreenApi.isFullScreen()) { // fullscreen mode has changed
+          if (wasFullScreen) {
+            $('body').removeClass('fullscreen');
+//            setMinZoom();
+//            map.fitBounds(L.latLngBounds(getActivePoints()), {
+//              pan: {
+//                animate: true
+//              },
+//              zoom: {
+//                animate: true
+//              },
+//              maxZoom: settings.zoom_max_filter
+//            });
+            // you have just EXITED full screen
+          } else {
+            $('body').addClass('fullscreen');
+//            setMinZoom();
+//            map.fitBounds(L.latLngBounds(getActivePoints()), {
+//              pan: {
+//                animate: true
+//              },
+//              zoom: {
+//                animate: true
+//              },
+//              maxZoom: settings.zoom_max_filter
+//            });
+            // you have just ENTERED full screen
+          }
+          wasFullScreen = fullScreenApi.isFullScreen();
+        }
+      }
+      // if not embedded treat as fullacreen
+      if (top === self) {
+        $('body').addClass('standalone');
+      }
+      $("a[data-toggle='fullscreen']").attr("href", map_url);
+      $("a[data-toggle='fullscreen']").click(function (e) {
+        // if embedded and fullscreen support
+        // also excluding webkit browsers for now
+        var webkit = /webkit/.test(navigator.userAgent.toLowerCase());
+        if (top !== self && fullScreenApi.supportsFullScreen && !webkit) {
+          e.preventDefault();
+          $('html').requestFullScreen();
+        }
+      });
+      $("a[data-toggle='fullscreen-close']").attr("href", parent_url);
+      $("a[data-toggle='fullscreen-close']").click(function (e) {
+        // if embedded and fullscreen support
+        if (top !== self && fullScreenApi.supportsFullScreen) {
+          e.preventDefault();
+          $('html').cancelFullScreen();
+        }
+      });
+    }
+  }    
       
 });
